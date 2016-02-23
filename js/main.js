e831037720ff82e477843b3a19b340e57dd5b537
Michelle <vand0432@algonquinlive.com>
2016-02-23 15:09:22 -0500

    updating.  Need to figure out how to break lines

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 var theLetter = prompt('Enter a letter.');
 
 
+var makeTriangle = function (name, times) {
+  var i;
+
+  for (i = 0; i < times; i++) {
+    document.write(name);
+  }
+};
+
+
+makeTriangle (theLetter, 1);
+makeTriangle (theLetter, 2);
+makeTriangle (theLetter, 3);
+makeTriangle (theLetter, 4);
+makeTriangle (theLetter, 5);
+makeTriangle (theLetter, 6);
+makeTriangle (theLetter, 7);
+makeTriangle (theLetter, 8);
+makeTriangle (theLetter, 9);
